1d0291bf559e5aeddac015888dcee137f30ce013
khupym <joel1991102@gmail.com>
2018-05-18 14:30:45 -0700

    add changebtn

--- reactjs_intro/src/App.js
+++ reactjs_intro/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import Hello from './Hello';
+import Changebtn from './Changebtn';
 import './App.css';
 
 class App extends Component {
@@ -15,6 +16,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Changebtn/>
       </div>
     );
   }
